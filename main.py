7f6becaf1833273df56db30cdc8707e298e3e28f
Iurii Dziuban <iurii.dziuban@gmail.com>
2018-01-29 09:09:44 +0200

    Add webhook decorator for dialogflow

--- main.py
+++ main.py
@@ -16,7 +16,7 @@
 import os
 import json
 import plotly
-from flask import Flask, render_template, url_for, request, redirect, flash, session
+from flask import Flask, render_template, url_for, request, redirect, flash, session, make_response
 from flask_wtf import Form
 from wtforms import FileField, StringField, SubmitField, validators
 from flask_wtf.csrf import CSRFProtect
@@ -198,6 +198,44 @@ def index():
 @csrf.exempt
 def not_found(e):
     return render_template('404.html')
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "foodcomposition":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    foodlabel = parameters.get("food")
+
+    myoutput = nutrionix_requests(foodlabel)['average_percents']
+    #cost = {'Andhra Bank':'6.85%', 'Allahabad Bank':'6.75%', 'Axis Bank':'6.5%', 'Bandhan bank':'7.15%', 'Bank of Maharashtra':'6.50%', 'Bank of Baroda':'6.90%', 'Bank of India':'6.60%', 'Bharatiya Mahila Bank':'7.00%', 'Canara Bank':'6.50%', 'Central Bank of India':'6.60%', 'City Union Bank':'7.10%', 'Corporation Bank':'6.75%', 'Citi Bank':'5.25%', 'DBS Bank':'6.30%', 'Dena Bank':'6.80%', 'Deutsche Bank':'6.00%', 'Dhanalakshmi Bank':'6.60%', 'DHFL Bank':'7.75%', 'Federal Bank':'6.70%', 'HDFC Bank':'5.75% to 6.75%', 'Post Office':'7.10%', 'Indian Overseas Bank':'6.75%', 'ICICI Bank':'6.25% to 6.9%', 'IDBI Bank':'6.65%', 'Indian Bank':'4.75%', 'Indusind Bank':'6.85%', 'J&K Bank':'6.75%', 'Karnataka Bank':'6.50 to 6.90%', 'Karur Vysya Bank':'6.75%', 'Kotak Mahindra Bank':'6.6%', 'Lakshmi Vilas Bank':'7.00%', 'Nainital Bank':'7.90%', 'Oriental Bank of Commerce':'6.85%', 'Punjab National Bank':'6.75%', 'Punjab and Sind Bank':'6.4% to 6.80%', 'Saraswat bank':'6.8%', 'South Indian Bank':'6% to 6.75%', 'State Bank of India':'6.75%', 'Syndicate Bank':'6.50%', 'Tamilnad Mercantile Bank Ltd':'6.90%', 'UCO bank':'6.75%', 'United Bank Of India':'6%', 'Vijaya Bank':'6.50%', 'Yes Bank':'7.10%'}
+
+    herewego = "Fats: {}, carbohydrates: {}, proteins: {}".format(myoutput[0], myoutput[1], myoutput[2])
+    print("Response:")
+    print(herewego)
+
+    return {
+        "speech": herewego,
+        "displayText": herewego,
+        #"data": {},
+        #"contextOut": [],
+        "source": "FoodComposition"
+    }
 
 
 # Run Flask server (host='0.0.0.0' - for Vagrant)
